cefbb07594ac11b5f243ea50107785b6cdeabf5d
cassianobrexbit <cassiano@brexbit.com>
2021-12-22 08:50:24 -0300

    Segundo commit

---
+++ src/lambda_file_metadata.py
@@ -0,0 +1,40 @@
+import json
+import boto3
+import uuid
+
+s3 = boto3.client('s3')
+ddb = boto3.resource('dynamodb')
+
+# Função para extração de metadados de arquivo enviado ao Amazon S3
+def lambda_handler(event,context):
+    table = ddb.Table("dio-live-step-tb")
+    
+    # Leitura da entrada da máquina de estado (state machine)
+    states_input = json.dumps(event)
+    get_bucket_values = json.loads(states_input)
+
+    try:
+
+        bucket = get_bucket_values["detail"]["requestParameters"]["bucketName"]
+        key = get_bucket_values["detail"]["requestParameters"]["key"]
+        
+        # Chamada do bucket S3
+        obj = s3.get_object(Bucket=bucket, Key=key)
+        newData = {
+            'id': str(uuid.uuid4().hex),
+            'bucket':  bucket,
+            'filename': key,
+            'filesize': int(obj['ContentLength']),
+            'contentType': obj['ContentType'],
+            'labelData':{},
+            'faceData':{},
+            }
+
+        # Inserir dados no DynamoDB
+        table.put_item(Item=newData)
+        
+        return newData
+        
+    except Exception as e:
+
+        raise e
---
+++ src/lambda_rekognition_api.py
@@ -0,0 +1,55 @@
+import boto3
+import json
+from decimal import Decimal
+
+rekognition = boto3.client('rekognition')
+s3 = boto3.client('s3')
+ddb = boto3.resource('dynamodb')
+
+# Detecção de faces na imagem
+def detect_faces(bucket, key):
+    response = rekognition.detect_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+    return response
+
+# Detecção de rótulos na imagem
+def detect_labels(bucket, key):
+    response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+    return response
+
+def lambda_handler(event, context):
+    
+    table = ddb.Table("dio-live-step-tb")
+    
+    # Obter objeto do Amazon S3
+    states_input = json.dumps(event)
+    get_input = json.loads(states_input)
+    bucket = get_input["detail"]["requestParameters"]["bucketName"]
+    key = get_input["detail"]["requestParameters"]["key"]
+
+    try:
+
+        # Chamada do método para reconhecimento de faces em uma imagem obtida do Amazon S3
+        face_detect = detect_faces(bucket, key)
+        faces_map = json.loads(json.dumps(face_detect), parse_float=Decimal)
+
+        # Chamada do método para detecção de rótulos em uma imagem obtida do Amazon S3
+        label_detect = detect_labels(bucket, key)
+        labels_map = json.loads(json.dumps(label_detect), parse_float=Decimal)
+        
+        # Atualizar a tabela do DynamoDB com os dados obtidos do Rekognition
+        table.update_item(
+            Key={
+                    'filename': key,
+            },
+            UpdateExpression='set labelData = :label, faceData = :face',
+            ExpressionAttributeValues={
+                ':label': labels_map,
+                ':face':faces_map
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        return faces_map
+    except Exception as e:
+        print(e)
+        raise e
